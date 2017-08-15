5f825fdadc66dff2ceb836a09b0c431c58527866
Dave Overton <dave@sym.ph>
2017-08-15 14:26:57 +0800

    update to credentials

---
+++ sendgrid/credentials.py
@@ -0,0 +1,5 @@
+SENDGRID_API_KEY = ('INSERT API KEY'
+                    'INSERT SECRET')
+# This is will appear to originate from for your recipient
+FROM_NAME = 'Geostore'
+FROM_EMAIL = 'youremail@domain.com'
